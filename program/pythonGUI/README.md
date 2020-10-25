a94ba66e1b444c895ac03ba98915b569601c534c
JMbaozi <2316705476@qq.com>
2020-10-25 12:20:20 +0800

    1

---
+++ program/pythonGUI/README.md
@@ -0,0 +1,1 @@
+#### Python GUI模块Tkinter
---
+++ program/pythonGUI/test.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from tkinter import *
+
+
+# root = tk.Tk()
+# root.title("TestGUIdemo")
+# theLabel = tk.Label(root,text="Tkinter窗口程序")
+# theLabel.pack()
+# root.mainloop()
+
+
+# class App:
+#     def __init__(self,root):
+#         frame = tk.Frame(root)#框架
+#         frame.pack(side=tk.LEFT,padx=10,pady=10)#自动调整尺寸(可自定义)
+#         self.hi_there = tk.Button(frame,text="打招呼",bg="black",fg="white",command=self.say_hi)#bg():背景色，fg():字体颜色
+#         self.hi_there.pack(side=tk.LEFT)
+#     def say_hi(self):
+#         print("Hello,I am JMbaozi.")
+# root = tk.Tk()
+# app = App(root)
+# root.mainloop()
+
+
+# root = Tk()
+# textLabel = Label(root,text="你所下载的影片含有未成年人限制内容，\n请满18周岁后再单击观看！",
+#                 justify=LEFT,
+#                 padx=10)
+# textLabel.pack(side=LEFT)
+# photo = PhotoImage(file='img/bizhi.png')
+# imgLabel = Label(root,image=photo)
+# imgLabel.pack(side=RIGHT)
+# mainloop()
+
+
+# root = Tk()
+# photo = PhotoImage(file="img/bizhi.png")
+# theLabel = Label(root,
+#                 text="我是\nJMbaozi",
+#                 justify=LEFT,
+#                 image=photo,
+#                 compound=CENTER,#设置文本和图像的混合模式
+#                 font=('华康少女字体',20),
+#                 fg="white")
+# theLabel.pack()
+# mainloop()
+
