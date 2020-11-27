3c420cc8295fbd443d4e0b5162102b020e0279e3
JMbaozi <2316705476@qq.com>
2020-11-27 22:22:53 +0800

    update

--- program/空间分析实验/test.py
+++ program/空间分析实验/test.py
@@ -1,6 +1,22 @@
+import time
+from functools import reduce
 
-x = [1.3,2,3,4,5,6,7,8,9,10.7]
-print(min(x))
-print(int(max(x)))
-for i in range(10,20):
-    print(i)
+
+# x = [1.3,2,3,4,5,6,7,8,9,10.7]
+# print(min(x))
+# print(int(max(x)))
+# for i in range(10,20):
+#     print(i)
+
+sum1 = 0
+t1 = time.time()
+# for i in range(10000000):
+#     sum1 += i * i
+sum1 = reduce(lambda x,y:x + y , map(lambda x:x*x ,range(10000000)))
+t2 = time.time()
+T1 = t2 - t1
+
+
+print("%d用时：%f" % (sum1,T1))
+
+
