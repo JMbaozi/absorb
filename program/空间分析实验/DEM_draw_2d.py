f20b1fdd62b7a9cd1fc2835e27b69cc8bca9bc2e
JMbaozi <2316705476@qq.com>
2020-12-02 18:22:08 +0800

    update

--- program/空间分析实验/DEM_draw_2d.py
+++ program/空间分析实验/DEM_draw_2d.py
@@ -1,10 +1,10 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+# import numpy as np
 
 
-# https://www.bilibili.com/read/cv5956129/
-color_grade = ['lightblue','cyan','c','darkcyan','darkslategrey']
+# # https://www.bilibili.com/read/cv5956129/
+# color_grade = ['lightblue','cyan','c','darkcyan','darkslategrey']
 
-def IDW_draw_2d_height(x,y,g):
-    plt.scatter(x,y,color=g)
+# def IDW_draw_2d_height(x,y,g):
+#     plt.scatter(x,y,color=g)
