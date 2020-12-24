0b87b7ce3531b21b0f43c5303cebf07fb23f9eff
JMbaozi <2316705476@qq.com>
2020-12-24 18:45:35 +0800

    有权无向图最小生成树算法

--- program/空间分析实验/test.py
+++ program/空间分析实验/test.py
@@ -114,22 +114,193 @@
 
 
 
-from shapely.geometry import Point
-from shapely.geometry import LineString
-from shapely.geometry import MultiPolygon
-import matplotlib.pyplot as plt
-# 定义线段
-line = LineString([(0.1, 0.1), (2, 3),(3,3),(4,5)])
-# 生成缓冲区
-buffer = line.buffer(0.5)
-x1,y1=line.xy
+# from shapely.geometry import Point
+# from shapely.geometry import LineString
+# from shapely.geometry import MultiPolygon
+# import matplotlib.pyplot as plt
+# # 定义线段
+# line = LineString([(0.1, 0.1), (2, 3),(3,3),(4,5)])
+# # 生成缓冲区
+# buffer = line.buffer(0.5)
+# x1,y1=line.xy
  
-x2,y2=buffer.boundary.xy
+# x2,y2=buffer.boundary.xy
  
-plt.figure()
+# plt.figure()
  
-plt.plot(x1,y1)
+# plt.plot(x1,y1)
  
-plt.plot(x2,y2)
+# plt.plot(x2,y2)
  
-plt.show()
+# plt.show()v
+
+
+
+# from Structure.graph.WeightedUndigraph import WeightedUndigraph
+# from Structure.graph.Edge import Edge
+# from Structure.PriorityQueue.IndexMinpriorutyQueue import IndexMinPriorityQueue
+# from math import inf
+
+
+# class PrimMST:
+#     def __init__(self, graph):
+#         """MST here represent the Minimum Spanning Tree of the current loop"""
+#         self.graph = graph
+#         # Memorize the cheapest edge to MST of each vertex(index)
+#         self.min_edge_to_MST = [None for _ in range(self.graph.get_num_vertices())]
+#         # Store the smallest weight of each vertex(index)'s edge to MST;
+#         # Initialize it with infinite plus, we will compare out a minimum weight after
+#         self.min_weight_to_MST = [+inf for _ in range(self.graph.get_num_vertices())]
+#         # Mark a True if a vertex(index) has been visited
+#         self.marked = [False for _ in range(self.graph.get_num_vertices())]
+#         # Memorize the smaller weight of each vertex(index)'s edge connected to MST
+#         self.the_cut_edges = IndexMinPriorityQueue(self.graph.get_num_vertices())
+
+#         # Initialize a 0.0 as the minimum weight to weight_to_MST
+#         self.min_weight_to_MST[0] = 0.0
+#         self.the_cut_edges.insert(0, 0.0)
+#         while not self.the_cut_edges.is_empty():
+#             # Take out the minimum-weighted vertex, and make a visit(update) for it
+#             self.visit(self.the_cut_edges.delete_min_and_get_index())
+
+#     def visit(self, v):
+#         """Update the MST"""
+#         self.marked[v] = True
+#         for e in self.graph.adjacent_edges(v):
+#             w = e.opposite(v)
+#             # Check if the opposite vertex of v in edge e is marked, if did, skip this loop
+#             if self.marked[w]:
+#                 continue
+#             # Find out the minimum-weighted-edge vertex opposite to this vertex(v)
+#             if e.get_weight() < self.min_weight_to_MST[w]:    # e.get_weight():Get weight of the edge between v and w
+#                 # Update the minimum edge and weight
+#                 # print(f"v: {v}, w: {w}, min_weight_edge: {e.get_weight()}")
+#                 self.min_edge_to_MST[w] = e
+#                 self.min_weight_to_MST[w] = e.get_weight()
+#                 if self.the_cut_edges.is_index_exist(w):
+#                     # print(w, e.get_weight())
+#                     self.the_cut_edges.change_item(w, e.get_weight())
+#                 else:
+#                     self.the_cut_edges.insert(w, e.get_weight())
+
+#     def min_weight_edges(self):
+#         return [edge for edge in self.min_edge_to_MST if edge]
+
+
+# if __name__ == '__main__':
+
+#     with open('data/MST.txt', 'r') as f:
+#         num_vertices = int(f.readline())
+#         num_edges = int(f.readline())
+#         graph = WeightedUndigraph(num_vertices)
+
+#         for e in range(num_edges):
+#             v1, v2, w = f.readline().split()
+#             graph.add_edge(Edge(int(v1), int(v2), float(w)))
+#     P_MST = PrimMST(graph)
+
+#     for e in P_MST.min_weight_edges():
+#         v = e.either()
+#         w = e.opposite(v)
+#         weight = e.weight
+#         print(f"v: {v} w: {w} weight: {weight}")
+
+
+
+# PrioQueue = ()
+# def Prim(graph):
+#     vnum = graph.vertex_num()
+#     edges = PrioQueue((0,0,0))       #每次将新边加入到一个优先队列中
+#     mst = [None] * vnum              #用于判断边所连接的点是否已经遍历过
+#     edge_count = 0
+#     while edge_count < vnum and not edges.is_empty():
+#         weight, vi, vj = edges.dequeue()
+#         if mst[vj] == None:
+#             edge_count += 1
+#             mst[vj] = (vi, weight)
+#             for i,w in graph.out_edges(vj):   #将新点的出边加入优先队列
+#                 if not mst[i]:
+#                     edges.enqueue((w, vj, i))
+#     return mst
+
+
+
+#coding=utf-8
+class Graph(object):
+  def __init__(self, maps):
+    self.maps = maps
+    self.nodenum = self.get_nodenum()
+    self.edgenum = self.get_edgenum()
+ 
+  def get_nodenum(self):
+    return len(self.maps)
+ 
+  def get_edgenum(self):
+    count = 0
+    for i in range(self.nodenum):
+      for j in range(i):
+        if self.maps[i][j] > 0 and self.maps[i][j] < 9999:
+          count += 1
+    return count
+ 
+  def kruskal(self):
+    res = []
+    if self.nodenum <= 0 or self.edgenum < self.nodenum-1:
+      return res
+    edge_list = []
+    for i in range(self.nodenum):
+      for j in range(i,self.nodenum):
+        if self.maps[i][j] < 9999:
+          edge_list.append([i, j, self.maps[i][j]])#按[begin, end, weight]形式加入
+    edge_list.sort(key=lambda a:a[2])#已经排好序的边集合
+    
+    group = [[i] for i in range(self.nodenum)]
+    for edge in edge_list:
+      for i in range(len(group)):
+        if edge[0] in group[i]:
+          m = i
+        if edge[1] in group[i]:
+          n = i
+      if m != n:
+        res.append(edge)
+        group[m] = group[m] + group[n]
+        group[n] = []
+    return res
+ 
+  def prim(self):
+    res = []
+    if self.nodenum <= 0 or self.edgenum < self.nodenum-1:
+      return res
+    res = []
+    seleted_node = [0]
+    candidate_node = [i for i in range(1, self.nodenum)]
+    
+    while len(candidate_node) > 0:
+      begin, end, minweight = 0, 0, 9999
+      for i in seleted_node:
+        for j in candidate_node:
+          if self.maps[i][j] < minweight:
+            minweight = self.maps[i][j]
+            begin = i
+            end = j
+      res.append([begin, end, minweight])
+      seleted_node.append(end)
+      candidate_node.remove(end)
+    return res
+ 
+max_value = 9999
+row0 = [0,7,max_value,max_value,max_value,5]
+row1 = [7,0,9,max_value,3,max_value]
+row2 = [max_value,9,0,6,max_value,max_value]
+row3 = [max_value,max_value,6,0,8,10]
+row4 = [max_value,3,max_value,8,0,4]
+row5 = [5,max_value,max_value,10,4,0]
+maps = [row0, row1, row2,row3, row4, row5]
+graph = Graph(maps)
+print('邻接矩阵为\n%s'%graph.maps)
+print('节点数据为%d，边数为%d\n'%(graph.nodenum, graph.edgenum))
+print('------最小生成树kruskal算法------')
+print(graph.kruskal())
+print('------最小生成树prim算法')
+print(graph.prim())
+
