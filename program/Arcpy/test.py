d1bcd7fc55b4f8232c780e6ef9d442395d391183
JMbaozi <2316705476@qq.com>
2021-05-12 18:07:26 +0800

    Delete code.docx
    update

--- program/Arcpy/test.py
+++ program/Arcpy/test.py
@@ -119,3 +119,102 @@
 #     print(shuzi)
 #     print(fuhao)
 # judge("4452144dfsadfsdf")
+
+
+# import arcpy
+# arcpy.env.workspace = r"E:\ArcPyStudy\Data\ArcpyBook\data\CityOfSanAntonio.gdb"
+# try:
+#     qry = '"SVCAREA" = \'North\''
+#     flayer = arcpy.MakeFeatureLayer_management("Burglary","Burglary_Layer")
+#     arcpy.SelectLayerByAttribute_management(flayer)
+#     cnt = arcpy.GetCount_management(flayer)
+#     print("The number of selected records is:" + str(cnt))
+# except:
+#     print("Error")
+
+
+# import arcpy
+# arcpy.env.workspace = r"E:\ArcPyStudy\Data\ArcpyBook\data\CityOfSanAntonio.gdb"
+# try:
+#     flayer = arcpy.MakeFeatureLayer_management("Burglary","Burglary_Layer")
+#     arcpy.SelectLayerByLocation_management(flayer,"COMPLETELY_WITHIN",r"E:\ArcPyStudy\Data\ArcpyBook\Ch7\EdgewoodSD.shp")
+#     cnt = arcpy.GetCount_management(flayer)
+#     print("The number of selected records is:" + str(cnt))
+# except:
+#     print("Error")
+
+
+# import arcpy
+# arcpy.env.workspace = r"E:\ArcPyStudy\Data\ArcpyBook\data\CityOfSanAntonio.gdb"
+# try:
+#     qry = '"DOW" = \'Mon\''
+#     flayer = arcpy.MakeFeatureLayer_management("Burglary","Burglary_Layer")
+#     arcpy.SelectLayerByLocation_management(flayer,"COMPLETELY_WITHIN",r"E:\ArcPyStudy\Data\ArcpyBook\Ch7\EdgewoodSD.shp")
+#     arcpy.SelectLayerByAttribute_management(flayer,"SUBSET_SELECTION",qry)
+#     cnt = arcpy.GetCount_management(flayer)
+#     print("The total number of selected records is:" + str(cnt))
+# except Exception as e:
+#     print(e.message)
+
+
+# import arcpy.da as da,os
+# os.chdir(r"E:\ArcPyStudy\Data\ArcpyBook\data\CityOfSanAntonio.gdb")
+# print("os.walk:")
+# for dirpath,dirnames,filenames in os.walk(os.getcwd()):
+#     for filename in filenames:
+#         print(filename)
+
+# print("arcpy.da.Walk:")
+# for dirpath,dirnames,filenames in da.Walk(os.getcwd(),datatype="FeatureClass"):
+#     for filename in filenames:
+#         print(os.path.join(dirpath,filename))
+
+
+# import arcpy
+# arcpy.env.workspace = r"E:\ArcPyStudy\Data\ArcpyBook\data\CityOfSanAntonio.gdb"
+# fcList = arcpy.ListFeatureClasses('C*','Polygon')   # C开头且数据类型为Polygon的要素类
+# for fc in fcList:
+#     print(fc)
+
+
+# import arcpy
+# arcpy.env.workspace = r"E:\ArcPyStudy\Data\ArcpyBook\data\CityOfSanAntonio.gdb"
+# fieldList = arcpy.ListFields('Burglary')
+# for fld in fieldList:
+#     print('%s is a type of %s with a length of %i' % (fld.name,fld.type,fld.length))
+
+
+# import arcpy
+# arcpy.env.workspace = r"E:\ArcPyStudy\Data\ArcpyBook\New_Data\CityOfSanAntonio_Personal.mdb"
+# try:
+#     descFC = arcpy.Describe("SchoolDistricts")
+#     print("The shape type is: " + descFC.ShapeType)
+#     flds = descFC.fields
+#     for fld in flds:
+#         print("Field: " + fld.name)
+#         print("Type: " + fld.type)
+#         print("Length: " + str(fld.length))
+#     ext = descFC.extent
+#     print("XMin: %f" % ext.XMin)
+#     print("YMin: %f" % ext.YMin)
+#     print("XMax: %f" % ext.XMax)
+#     print("YMax: %f" % ext.YMax)
+# except:
+#     print(arcpy.GetMessages)
+
+
+# import arcpy
+# arcpy.env.workspace = r"E:\ArcPyStudy\Data\ArcpyBook\data"
+# try:
+#     descRaster = arcpy.Describe("AUSTIN_EAST_NW.sid")
+#     ext = descRaster.extent
+#     print("XMin: %f" % ext.XMin)
+#     print("YMin: %f" % ext.YMin)
+#     print("XMax: %f" % ext.XMax)
+#     print("YMax: %f" % ext.YMax)
+
+#     sr = descRaster.SpatialReference
+#     print("name: " + sr.name)
+#     print("type: " + sr.type)
+# except Exception as e:
+#     print(e.message)
