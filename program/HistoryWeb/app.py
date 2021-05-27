c4fefeb13a3194f02a74905349abf30561718ac6
JMbaozi <2316705476@qq.com>
2021-05-27 17:10:47 +0800

    Update app.cpython-36.pyc

--- program/HistoryWeb/app.py
+++ program/HistoryWeb/app.py
@@ -8,32 +8,41 @@ app = Flask(__name__)
 
 ####################################################
 Dynastys = ['东魏','隋代','唐','北宋','刘宋','金','元','明','清']# 各朝代列表
+Countys = []# 各朝所含郡县
+Countynum = []# 各朝所含郡县数量
 Households = []# 各朝代总户数
 Populations = []# 各朝代总人口总数
 Families = []# 各朝代人每户数量
 Household_dict = {}# 户数字典
 Population_dict = {}# 人口字典
 Familie_dict = {}# 人每户字典
-
+####################################################
 # 连接数据库
 db = sqlite3.connect('test\数据库\HISTORY_DATABASE')
 print("数据库连接成功")
 cur = db.cursor()
 for dynasty in Dynastys:
-    num1,num2,num3 = 0,0,0# 户数，口数，人每户
+    num1,num2,num3,num4 = 0,0,0,0# 户数，口数，人每户，郡县数
+    countys = []# 单个朝代的所有郡县名称
     data = cur.execute("select * from people where 朝代='{}'".format(dynasty))
     for row in data:
         num1 += int(row[3])
         num2 += int(row[4])
         num3 += float(row[5])
+        num4 += 1
+        countys.append(str(row[2]))
     Households.append(num1)
     Populations.append(num2)
-    Families.append(float('%.1f' % num3))# 结果保留一位小数
+    Families.append(float('%.2f' % float(num3/num4)))# 结果保留一位小数
+    Countynum.append(num4)
+    Countys.append(countys)
     print("{}计算成功".format(dynasty))
 for i in range(len(Dynastys)):
     print("{}共有{}户".format(Dynastys[i],str(Households[i])))
     print("{}共有{}人".format(Dynastys[i],str(Populations[i])))
     print("{}每户平均有{}人".format(Dynastys[i],str(Families[i])))
+    print("{}共有{}个郡县".format(Dynastys[i],str(Countynum[i])))
+    print("{}的郡县有{}".format(Dynastys[i],str(Countys[i])))
 Household_dict = dict(list(zip(Dynastys,Households)))
 Population_dict = dict(list(zip(Dynastys,Populations)))
 Familie_dict = dict(list(zip(Dynastys,Families)))
@@ -42,6 +51,8 @@ print(Population_dict)
 print(Familie_dict)
 ####################################################
 
+
+
 @app.route('/')
 def hello_world():
     data=123123
@@ -49,14 +60,28 @@ def hello_world():
 
 @app.route('/people_all')
 def people_all():
-    xlabel = ['东魏','隋代','唐','北宋','刘宋','金','元','明','清']# 各朝代列表
-    ylabel = Populations# 各朝代总人口总数
-    return render_template("people_all.html",xlabel=xlabel,ylabel=ylabel)
+    Populationlabel = Populations# 各朝代总人口总数
+    Dynastylabel = ['东魏','隋代','唐','北宋','刘宋','金','元','明','清']# 各朝代列表
+    Householdlabel = Households# 各朝代总户数
+    familylabel = Families[:-1]# 各朝代人每户
+    Populationlabelweight = list(map(lambda x:x/1000000,Populationlabel))# 总人口加权除以1000000后的结果
+    Householdlabelweight = list(map(lambda x:x/1000000,Householdlabel))# 总户数加权除以1000000后的结果
+    Population_dict_sort = dict(sorted(Population_dict.items(),key=lambda item:item[1]))# 各朝代总人口从高到低排序
+    Dynasty_sort = list(Population_dict_sort.keys())# 排序后的朝代列表
+    Population_sort = list(Population_dict_sort.values())# 排序后的各朝代总人口数
+    return render_template("people_all.html",
+                            Dynasty_sort=Dynasty_sort,
+                            Population_sort=Population_sort,
+                            Populationlabel=Populationlabel,
+                            Populationlabelweight=Populationlabelweight,
+                            Dynastylabel=Dynastylabel,
+                            familylabel=familylabel,
+                            Householdlabel=Householdlabel,
+                            Householdlabelweight=Householdlabelweight)
 
 @app.route('/people')
 def people():
-    data=123123
-    return render_template("people.html",data=data)
+    return render_template("people.html",Population_dict=Population_dict)
 
 
 if __name__ == '__main__':
--- program/HistoryWeb/test/test.py
+++ program/HistoryWeb/test/test.py
@@ -1,4 +1,9 @@
-import sqlite3
-
-db = sqlite3.connect('数据库/HISTORY_DATABASE')
-cur = db.cursor()
+a = {'a' : 1, 'b': 2, 'c' : 3}
+ 
+#字典中的key转换为列表
+key_value = list(a.keys())
+print('字典中的key转换为列表：', key_value)
+ 
+#字典中的value转换为列表
+value_list = list(a.values())
+print('字典中的value转换为列表：', value_list)
