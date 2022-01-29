2db9969e030a4ba12fe3b67d1d8338509f782cee
JMbaozi <2316705476@qq.com>
2022-01-29 16:48:03 +0800

    Delete 1.py
    Study

---
+++ program/毕业设计/program/sutdy/study1.py
@@ -0,0 +1,54 @@
+import cv2 as cv
+import numpy as np
+
+
+def access_pixels(image):
+    print(image.shape)
+    height = image.shape[0]
+    width = image.shape[1]
+    channels = image.shape[2]
+    print("height: %s, width: %s, channels: %s" % (height,width,channels))
+    for row in range(height):
+        for col in range(width):
+            for c in range(channels):
+                pv = image[row,col,c]
+                image[row,col,c] = 255 - pv
+    cv.imshow("pixels_demo",image)
+
+def create_image():
+    """
+    img = np.zeros([400,400,3],np.uint8)
+    img[:,:,1] = np.ones([400,400])*255
+    cv.imshow("new image",img)
+
+    img = np.zeros([400,400,1],np.uint8)
+    img[:,:,0] = np.ones([400,400])*127
+    cv.imshow("new image",img)
+    cv.imwrite("img/creat_image.jpg",img)
+    """
+
+    m1 = np.ones([3,3],np.float32)
+    m1.fill(122.388)
+    print(m1)
+    m2 = m1.reshape([1,9])
+    print(m2)
+
+def inverse(image):
+    dst = cv.bitwise_not(image)
+    cv.imshow("inverse demo",dst)
+
+
+print("-----------------------------")
+src = cv.imread('img/picture4.jpg')#bule,green,red
+cv.namedWindow("input image",cv.WINDOW_AUTOSIZE)
+cv.imshow("input image",src)
+t1 = cv.getTickCount()
+create_image()
+inverse(src)
+# access_pixels(src)
+t2 = cv.getTickCount()
+time = (t2-t1)/cv.getTickFrequency();
+print("time: %s ms" % str(time*1000))
+cv.waitKey(0)
+
+cv.destroyAllWindows()
---
+++ program/毕业设计/program/sutdy/study2.py
@@ -0,0 +1,54 @@
+import cv2 as cv
+import numpy as np
+
+
+def extrace_object_demo():
+    capture = cv.VideoCapture("video/test.avi")
+    while(True):
+        ret,frame = capture.read()
+        if ret == False:
+            break
+        hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+        # check blue
+        lower_hsv = np.array([100,43,46])
+        upper_hsv = np.array([124,255,255])
+        mask = cv.inRange(hsv,lowerb=lower_hsv,upperb=upper_hsv)
+        cv.imshow("video",frame)
+        cv.imshow("mask",mask)
+        c = cv.waitKey(40)
+        if c == 27:
+            break
+
+def color_space_demo(image):
+    gray = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+    cv.imshow("gray",gray)
+    hsv = cv.cvtColor(image,cv.COLOR_BGR2HSV)
+    cv.imshow("hsv",hsv)
+    yuv = cv.cvtColor(image,cv.COLOR_BGR2YUV)
+    cv.imshow("yuv",yuv)
+    Ycrcb = cv.cvtColor(image,cv.COLOR_BGR2YCrCb)
+    cv.imshow("Ycrcb",Ycrcb)
+
+print("-----------------------------")
+src = cv.imread('img/picture4.jpg')#bule,green,red
+# cv.namedWindow("input image",cv.WINDOW_AUTOSIZE)
+# cv.imshow("input image",src)
+# color_space_demo(src)
+# extrace_object_demo()
+
+# 分离通道
+b,g,r = cv.split(src)
+cv.imshow("blue",b)
+cv.imshow("green",g)
+cv.imshow("red",r)
+
+src[:,:,2] = 0
+cv.imshow("changed image",src)
+
+# 合并通道
+src = cv.merge([b,g,r])#必须给定数组
+cv.imshow("changed img",src)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
